b6dab5be814214bdbd7c59cec513762e50481f08
Reed <reedbroadhead@gmail.com>
2023-05-17 22:48:26 -0500

    adds client

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Login from './components/Login.jsx'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <Login />
+  )
+}
+
+export default App
---
+++ client/src/components/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { useState } from 'react'
+
+function Login() {
+    
+    return (
+        <h1>hi there</h1>
+    )
+}
+
+export default Login;
